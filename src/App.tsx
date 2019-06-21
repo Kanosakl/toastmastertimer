70a17dacf21e4943feb39f06bd378d543db74a55
Davion Teh <davion.teh@afm.cc>
2019-06-21 21:24:40 +0800

    MyTimer checkpoint commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { MyTimer } from './mytimer.js';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export class App extends React.Component {
+
+  render() {
+    return (
+      <div className="App">
+        <MyTimer checkpoints={[{ time: msConverter({seconds:3}), callback: () => console.log("hi") }]} />
+      </div>
+    );
+  }
 }
 
-export default App;
+function msConverter({ milliseconds = 0, seconds = 0, minutes = 0, hours = 0 }:
+  { milliseconds?: number, seconds?: number, minutes?: number, hours?: number }) {
+    return milliseconds + (seconds * 1000) + (minutes * 60000) + (hours * 36000) 
+}
+
